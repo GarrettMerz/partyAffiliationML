0c21aec91dfd90416b13bed0c074387e80f51b3d
GarrettMerz <garrettwmerz@gmail.com>
2017-11-15 18:57:40 -0500

    Added Naive Bayes Classifier. Error on forGarrett set is 10.75268817204301

---
+++ NaiveBayes.py
@@ -0,0 +1,76 @@
+import numpy as np
+import heapq
+
+numspam = 0
+numgood = 0
+guessgoodfiles = 0
+guessbadfiles = 0
+tokens_size = 12650
+linenum = 0
+probwordgivspam, probwordgivgood, spamsum, goodsum, logprobs = [0]*tokens_size, [0]*tokens_size, [0]*tokens_size, [0]*tokens_size, [0]*tokens_size
+
+with open('partyAffiliationML/forGarrett/train_data.dat') as f:
+#get labels, calculate frequency sums
+ for i, line in enumerate(f):
+  label = line.split('  ')[0]
+  text = line.split('  ')[1]
+  for cell in text.strip().split(' '):
+   word, freq = cell.split(':')
+   if int(label) == 1:
+    spamsum[int(word)-1] += int(freq)
+    numspam += 1
+   if int(label) == -1:
+    goodsum[int(word)-1] += int(freq)
+    numgood += 1
+
+ #get probability of good & spam
+probspam = numspam / (numspam + numgood)
+probgood = numgood / (numspam + numgood)
+
+ #use frequency sums to get conditionals
+for i in range(0, tokens_size):
+ probwordgivspam[i] = (1 + spamsum[i])/(len(spamsum)+sum(spamsum))
+ probwordgivgood[i] = (1 + goodsum[i])/(len(goodsum)+sum(goodsum))
+ logprobs[i] = np.log(probwordgivspam[i]/probwordgivgood[i])
+
+with open('partyAffiliationML/forGarrett/test_data.dat') as g:
+ numdocs = 0
+ misclass = 0
+#get labels
+ for i, line in enumerate(g):
+  linenum += 1
+  numdocs += 1
+  guesslabel = 0
+  logpost = np.log(probspam/probgood)
+  testwords = []
+  testlabel = line.split('  ')[0]
+  testtext = line.split('  ')[1]
+  for cell in testtext.strip().split(' '):
+   testword, testfreq = cell.split(':')
+   testwords.append(int(testword)-1)
+  for test in testwords:
+   logpost += np.log(probwordgivspam[test]/probwordgivgood[test])
+  if (logpost > 0):
+   guesslabel = 1
+   guessgoodfiles += 1
+  elif (logpost < 0):
+   guesslabel = -1
+   guessbadfiles += 1
+  #if (int(testlabel) == guesslabel):
+   #print(int(testlabel))
+  if (int(testlabel) != guesslabel):
+   misclass += 1
+   print(linenum)
+
+error = float((misclass/numdocs)*100.0)
+
+print("error is")
+print(error)
+print("republican speeches number guess is")
+print(guessbadfiles)
+print("democrat speeches number guess is")
+print(guessgoodfiles)
+print("spammiest words are at")
+
+spammiest = heapq.nlargest(5, range(len(logprobs)), logprobs.__getitem__)
+print(spammiest + np.ones(len(spammiest)))
