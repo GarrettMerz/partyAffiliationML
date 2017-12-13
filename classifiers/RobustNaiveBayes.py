2a3e8102f8c01ea2f5dd3463809ebe01cf0a7e14
GarrettMerz <garrettwmerz@gmail.com>
2017-12-13 13:42:06 -0500

    fixing merge conflicts

--- classifiers/RobustNaiveBayes.py
+++ classifiers/RobustNaiveBayes.py
@@ -5,6 +5,8 @@ numspam = 0
 numgood = 0
 guessgoodfiles = 0
 guessbadfiles = 0
+robust_spam_count = 0
+robust_good_count = 0
 tokens_size = 12650
 linenum = 0
 probwordgivspam, probwordgivgood, spamsum, goodsum, logprobs = [0]*tokens_size, [0]*tokens_size, [0]*tokens_size, [0]*tokens_size, [0]*tokens_size
@@ -27,14 +29,29 @@ with open('../data/processed_wf/2008_2016/train_data.dat') as f:
 probspam = numspam / (numspam + numgood)
 probgood = numgood / (numspam + numgood)
 
- #use frequency sums to get conditionals
-for i in range(0, tokens_size):
- probwordgivspam[i] = (1 + spamsum[i])/(tokens_size+sum(spamsum))
- probwordgivgood[i] = (1 + goodsum[i])/(tokens_size+sum(goodsum))
- logprobs[i] = np.log(probwordgivspam[i]/probwordgivgood[i])
+#Remove Laplace Smoothing! Check tokens to see which ones aren't in train set
+#for i in range(0, tokens_size):
+ #if spamsum[i] == 0 && goodsum[i] == 0:
+  #robust_spam_count += 1
+ #if goodsum[i] == 0:
+  #robust_good_count += 1
 
- numdocs = 0
- misclass = 0
+
+#use frequency sums to get conditionals
+for i in range(0, tokens_size):
+  probwordgivspam[i] = (spamsum[i])/(sum(spamsum))
+  probwordgivgood[i] = (goodsum[i])/(sum(goodsum))
+
+# logprobs[i] = np.log(probwordgivspam[i]/probwordgivgood[i])
+
+numdocs = 0
+misclass = 0
+
+print("Number of tokens not in train set of R speeches is") 
+print(robust_spam_count)
+print("Number of tokens not in train set of D speeches is") 
+print(robust_good_count)
+
 #get labels
 with open('../data/processed_wf/2008_2016/test_data.dat') as g:
  for i, line in enumerate(g):
@@ -42,6 +59,8 @@ with open('../data/processed_wf/2008_2016/test_data.dat') as g:
   numdocs += 1
   guesslabel = 0
   logpost = np.log(probspam/probgood)
+  words_not_in_train_good = 0
+  words_not_in_train_spam = 0
   testwords = []
   testlabel = line.split('  ')[0]
   testtext = line.split('  ')[1]
@@ -49,7 +68,13 @@ with open('../data/processed_wf/2008_2016/test_data.dat') as g:
    testword, testfreq = cell.split(':')
    testwords.append(int(testword)-1)
   for test in testwords:
-   logpost += np.log(probwordgivspam[test]/probwordgivgood[test])
+   if (probwordgivspam[test] == 0) && (probwordgivgood[test] == 0):
+    if (probwordgivspam[test] == 0):
+     words_not_in_train_good += 1
+    if (probwordgivgood[test] == 0):
+     words_not_in_train_spam += 1
+   
+ else logpost += np.log(probwordgivspam[test]/probwordgivgood[test])
   if (logpost > 0):
    guesslabel = 1
    guessgoodfiles += 1
@@ -64,17 +89,16 @@ with open('../data/processed_wf/2008_2016/test_data.dat') as g:
 
 error = float((misclass/numdocs)*100.0)
 
-print("error is")
-print(error)
-print("republican speeches number guess is")
-print(guessbadfiles)
-print("democrat speeches number guess is")
-print(guessgoodfiles)
-print("spammiest words are at")
+#print("error is")
+#print(error)
+#print("republican speeches number guess is")
+#print(guessbadfiles)
+#print("democrat speeches number guess is")
+#print(guessgoodfiles)
+#print("spammiest words are at")
 
-spammiest = heapq.nlargest(5, range(len(logprobs)), logprobs.__getitem__)
-print(spammiest + np.ones(len(spammiest)))
 
-unspammiest = heapq.nlargest(5, range(len(logprobs)), -1*logprobs.__getitem__)
-print(unspammiest + np.ones(len(unspammiest)))
 
+
+#spammiest = heapq.nlargest(5, range(len(logprobs)), logprobs.__getitem__)
+#print(spammiest + np.ones(len(spammiest)))
